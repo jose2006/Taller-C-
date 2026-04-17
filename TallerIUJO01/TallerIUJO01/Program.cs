d1295eff5da8fd05fd2e92ec4a3dfb459d652a43
jose2006 <joseyepez2006.jy@gmail.com>
2026-04-17 11:30:06 -0400

    agragando manipulacion de texto

--- TallerIUJO01/TallerIUJO01/Program.cs
+++ TallerIUJO01/TallerIUJO01/Program.cs
@@ -14,9 +14,23 @@ namespace TallerIUJO01
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("taller 01");
 			
-			// TODO: Implement Functionality Here
+			string registroUsuario = " ID_777; juan; evaluacion; 95 ";
+			
+			
+			Console.WriteLine(registroUsuario);
+			string registroLimpio = registroUsuario.Trim();
+			Console.WriteLine(registroLimpio);
+			
+			string[] partes = registroLimpio.Split(';');
+			string id = partes[0].Trim();
+			string nombre = partes[1].Trim();
+			string tarea = partes[2].Trim();
+			string nota = partes[3].Trim();
+			
+			
+			Console.WriteLine(string.Format(" el id es: {0} del usuario {1} en la tarea {2} con la nota {3}",id,nombre,tarea,nota));
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
